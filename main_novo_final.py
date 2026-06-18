9ef23d5def26acf53ac703846a1e5525d05f8ffc
ydssm <134299051+yasminsiqueira@users.noreply.github.com>
2026-06-17 23:44:41 -0300

    Update main_novo_final.py

--- main_novo_final.py
+++ main_novo_final.py
@@ -16,7 +16,7 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel('models/gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-001')
 
 app = FastAPI(title="Locus Aura - Ecossistema Imobiliário Oficial")
 
